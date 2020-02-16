3919d98ccc36955810e693788e8b7dbf52777475
leejiwonn <37164467+leejiwonn@users.noreply.github.com>
2020-02-16 22:14:25 +0900

    Sidebar 컴포넌트 css 수정

--- src/containers/Sidebar/style.js
+++ src/containers/Sidebar/style.js
@@ -11,6 +11,31 @@ export const ButtonWrapper = styled.div`
   margin: 45px auto;
 `;
 
+export const SideBtn = styled.div`
+  width: 50px;
+  height: 50px;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 50px;
+  letter-spacing: normal;
+  text-align: center;
+  color: #777777;
+  background: #f5f5f5;
+  border-radius: 50px;
+  margin-bottom: 50px;
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+    color: #383b41;
+  }
+
+  ${p => p.isActive ? `
+    font-weight: bold;
+    color: #ff595f;
+  ` : ''};
+`;
+
 export const Img = styled.img`
   width: 50px;
   height: 50px;
