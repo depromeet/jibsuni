5c191c0ca71bfbe770a672fb33dae5277c509a08
leejiwonn <37164467+leejiwonn@users.noreply.github.com>
2020-02-22 00:52:21 +0900

    Room Dialog 연동중

--- src/components/Room/RoomTemplate/index.jsx
+++ src/components/Room/RoomTemplate/index.jsx
@@ -1,24 +1,44 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { getFurnituresAPI, getRoomsAPI } from '../../../utils/api';
 
+import Dialog from '../../../components/Dialog';
+import CreateTask from '../../../containers/CreateTask';
 import Bedroom from '../Bedroom';
 import Bathroom from '../Bathroom';
 import Kitchen from '../Kitchen';
 
 function Room() {
   const selectedRoomType = useSelector(state => state.room.selectedRoomType);
+  const selectedRoomId = useSelector(state => state.room.selectedRoomId);
+  const token = useSelector(state => state.auth.token);
+
+  const [show, setShow] = useState(false);
+  const handleClick = useCallback(() => {
+    setShow(prev => !prev);
+    if(selectedRoomId == "ALL"){
+      getRoomsAPI(token);
+    }else{
+      getFurnituresAPI(token, selectedRoomId);
+    }
+  }, [token, selectedRoomId]);
   
   return (
     <>
       {
         (function() {
-          if(selectedRoomType == "all") return (<Bedroom />);
-          else if(selectedRoomType == "bedroom") return (<Bedroom />);
-          else if(selectedRoomType == "living-room") return (<Bedroom />);
-          else if(selectedRoomType == "kitchen") return (<Kitchen />);
-          else if(selectedRoomType == "bathroom") return (<Bathroom />);
+          if(selectedRoomType == "all") return (<Bedroom handleClick={handleClick} />);
+          else if(selectedRoomType == "bedroom") return (<Bedroom handleClick={handleClick} />);
+          else if(selectedRoomType == "living-room") return (<Bedroom handleClick={handleClick} />);
+          else if(selectedRoomType == "kitchen") return (<Kitchen handleClick={handleClick} />);
+          else if(selectedRoomType == "bathroom") return (<Bathroom handleClick={handleClick} />);
         })()
       }
+      <Dialog show={show} handleClose={handleClick}>
+        <CreateTask
+          // room={selectedRoom}
+        />
+      </Dialog>
     </>
   )
 }
