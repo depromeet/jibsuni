64cc1448cfc7a7d2b59f4554fa5303f9047a40ed
leejiwonn <37164467+leejiwonn@users.noreply.github.com>
2020-02-22 00:51:51 +0900

    furnitureType 수정중

--- src/constants/furnitureType.js
+++ src/constants/furnitureType.js
@@ -1,6 +1,14 @@
 export const FurnitureType = {
-  bedroom: 'bedroom',
-  livingRoom: 'living-room',
-  kitchen: 'kitchen',
-  bathroom: 'bathroom',
+  주방: 'kitchen',
+  침실: 'bedroom',
+  거실: 'living-room',
+  화장실: 'bathroom',
+};
+
+export const furnitureNameByType = {
+  all: '전체',
+  kitchen: '주방',
+  bedroom: '침실',
+  'living-room': '거실',
+  bathroom: '화장실',
 };
